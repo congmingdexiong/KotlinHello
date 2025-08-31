4dd9ffebfd82db2d5471777f62c75f6558f77eda
congmingdexiong <ltlobygg@163.com>
2025-08-31 21:09:24 +0800

    init

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+//    println("Hello World!")
+//
+//    var count:Int = 2
+//    println(count)
+//
+//    val a:Boolean  = true
+//    println(a)
+//
+//    var b = count in 0 .. 100
+//    println(b)
+
+
+    var str:String ="""
+        
+        hello world
+    """.trimIndent()
+
+
+    val c = "${str}is good"
+    println(c)
+}
